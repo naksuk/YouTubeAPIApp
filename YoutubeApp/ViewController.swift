050ded5999da8eecde841ec536777366d086968e
“naksuk <n4ksuk@gmail.com>
2021-07-29 21:24:05 +0900

    ep1

--- YoutubeApp/ViewController.swift
+++ YoutubeApp/ViewController.swift
@@ -1,19 +1,40 @@
-//
-//  ViewController.swift
-//  YoutubeApp
-//
-//  Created by 中村亮介 on 2021/07/29.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var videoListCollectionView: UICollectionView!
+    
+    private let cellId = "cellId"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        videoListCollectionView.backgroundColor = .red
+        videoListCollectionView.delegate = self
+        videoListCollectionView.dataSource = self
+        
+        videoListCollectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: cellId)
     }
-
 
 }
 
+extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width = self.view.frame.width
+        
+        return .init(width: width, height: width)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = videoListCollectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
+        cell.backgroundColor = .green
+        return cell
+    }
+    
+    
+        
+}
