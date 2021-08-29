b43f92929e77b95d9a03e363d33ee324e3363e86
“naksuk <n4ksuk@gmail.com>
2021-08-29 14:42:03 +0900

    スワイプアニメーションの挙動を修正

--- YoutubeApp/ViewControllers/BaseTabBarController.swift
+++ YoutubeApp/ViewControllers/BaseTabBarController.swift
@@ -1,43 +1,42 @@
 import UIKit
 
-enum ControllerName: Int {
-    case home, search, channel, inbox, library
-}
-
 class BaseTabBarController: UITabBarController {
+    
+    enum ControllerName: Int {
+        case home, search, channel, inbox, library
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        setupViewController()
-
         
+        
+        setupViewControllers()
     }
     
-    private func setupViewController() {
+    private func setupViewControllers() {
         viewControllers?.enumerated().forEach({ (index, viewController) in
             if let name = ControllerName.init(rawValue: index) {
                 switch name {
                 case .home:
-                    setTabbbarInfo(viewController, selectedImageName: "good-selected", unselectedImageName: "good-unselected", title: "ホーム")
+                    setTabbarInfo(viewController, selectedImageName: "home-icon-selected", unselectedImageName: "home-icon-unselected", title: "ホーム")
                 case .search:
-                    setTabbbarInfo(viewController, selectedImageName: "good-selected", unselectedImageName: "good-unselected", title: "検索")
+                    setTabbarInfo(viewController, selectedImageName: "search-icon-selected", unselectedImageName: "search-icon-unselected", title: "検索")
                 case .channel:
-                    setTabbbarInfo(viewController, selectedImageName: "good-selected", unselectedImageName: "good-unselected", title: "登録チャンネル")
+                    setTabbarInfo(viewController, selectedImageName: "channel-icon-selected", unselectedImageName: "channel-icon-unselected", title: "登録チャンネル")
                 case .inbox:
-                    setTabbbarInfo(viewController, selectedImageName: "good-selected", unselectedImageName: "good-unselected", title: "受信トレイ")
+                    setTabbarInfo(viewController, selectedImageName: "inbox-icon-selected", unselectedImageName: "inbox-icon-unselected", title: "受信トレイ")
                 case .library:
-                    setTabbbarInfo(viewController, selectedImageName: "good-selected", unselectedImageName: "good-unselected", title: "ライブラリ")
+                    setTabbarInfo(viewController, selectedImageName: "library-icon-selected", unselectedImageName: "library-icon-unselected", title: "ライブラリ")
                 }
             }
         })
     }
     
-    
-    private func setTabbbarInfo(_ viewController: UIViewController, selectedImageName: String, unselectedImageName: String, title: String) {
+    private func setTabbarInfo(_ viewController: UIViewController, selectedImageName: String, unselectedImageName: String, title: String) {
         viewController.tabBarItem.selectedImage = UIImage(named: selectedImageName)?.resize(size: .init(width: 20, height: 20))?.withRenderingMode(.alwaysOriginal)
         viewController.tabBarItem.image = UIImage(named: unselectedImageName)?.resize(size: .init(width: 20, height: 20))?.withRenderingMode(.alwaysOriginal)
         viewController.tabBarItem.title = title
-        
     }
+    
     
 }
--- YoutubeApp/ViewControllers/VideoViewController.swift
+++ YoutubeApp/ViewControllers/VideoViewController.swift
@@ -5,55 +5,58 @@ class VideoViewController: UIViewController {
     
     var selectedItem: Item?
     private var imageViewCenterY: CGFloat?
+    
     var videoImageMaxY: CGFloat {
-        let ecloudValue = view.safeAreaInsets.bottom + (imageViewCenterY ?? 0)
-        return view.frame.maxY - ecloudValue
+        let ecludeValue = view.safeAreaInsets.bottom + (imageViewCenterY ?? 0)
+        return view.frame.maxY - ecludeValue
     }
     
-    var minimusImageViewTrailingConstant: CGFloat {
+    var minimumImageViewTrailingConstant: CGFloat {
         -(view.frame.width - (150 + 12))
     }
     
-    //videoImageView
+    // videoImageView
     @IBOutlet weak var videoImageView: UIImageView!
     @IBOutlet weak var videoImageViewHeightConstraint: NSLayoutConstraint!
     @IBOutlet weak var videoImageViewLeadingConstraint: NSLayoutConstraint!
     @IBOutlet weak var videoImageViewTrailingConstraint: NSLayoutConstraint!
     @IBOutlet weak var videoImageBackView: UIView!
     
-    //backView
+    // backView
     @IBOutlet weak var backView: UIView!
     @IBOutlet weak var backViewTopConstraint: NSLayoutConstraint!
     @IBOutlet weak var backViewTrailingConstraint: NSLayoutConstraint!
     @IBOutlet weak var backViewBottomConstraint: NSLayoutConstraint!
-    //describeView
+    
+    // describeView
     @IBOutlet weak var describeView: UIView!
     @IBOutlet weak var describeViewTopConstraint: NSLayoutConstraint!
-    
-    //ohters
-    @IBOutlet weak var videoTitleLabel: UILabel!
-    @IBOutlet weak var baseBackGroundView: UIView!
-    @IBOutlet weak var channelTitleLabel: UILabel!
+
     @IBOutlet weak var channelImageView: UIImageView!
-    
+    @IBOutlet weak var videoTitleLabel: UILabel!
+    @IBOutlet weak var channelTitleLabel: UILabel!
+    @IBOutlet weak var baseBackGroundView: UIView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        setupViews()
         
+        setupViews()
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
+        
         UIView.animate(withDuration: 0.3) {
             self.baseBackGroundView.alpha = 1
         }
+        
     }
-    
     
     private func setupViews() {
         self.view.bringSubviewToFront(videoImageView)
         
         imageViewCenterY = videoImageView.center.y
+        
         channelImageView.layer.cornerRadius = 45 / 2
         
         if let url = URL(string: selectedItem?.snippet.thumbnails.medium.url ?? "") {
@@ -67,12 +70,10 @@ class VideoViewController: UIViewController {
         videoTitleLabel.text = selectedItem?.snippet.title
         channelTitleLabel.text = selectedItem?.channel?.items[0].snippet.title
         
-        let panGesture = UIPanGestureRecognizer(target: self, action: #selector(panVideoImageView))
-        videoImageView.addGestureRecognizer(panGesture)
-        
+        let panGeture = UIPanGestureRecognizer(target: self, action: #selector(panVideoImageView))
+        videoImageView.addGestureRecognizer(panGeture)
     }
     
-    //videoImageViewのスワイプ移動
     @objc private func panVideoImageView(gesture: UIPanGestureRecognizer) {
         
         guard let imageView = gesture.view else { return }
@@ -91,37 +92,37 @@ class VideoViewController: UIViewController {
             // 左右のpadding設定
             self.adjustPaddingChange(move: move)
             
-            //imageViewの高さの動き
+            // imageViewの高さの動き
+            // 280(最大値) - 70(最小値) = 210
             self.adjustHeightChange(move: move)
-            
-            //alpha値の設定
+
+            // alpha値の設定
             self.adjustAlphaChange(move: move)
 
-            // imageViewの横幅の動き150(最小値)
-            self.adjustAlphaChange(move: move)
-            
+            // imageViewの横幅の動き 150(最小値)
+            self.adjustWidthChange(move: move)
             
         } else if gesture.state == .ended {
+            
             self.imageViewEndedAnimation(move: move, imageView: imageView as! UIImageView)
+
         }
     }
     
-    
-    // MARK: imageViewのPanGestureのstatusが、[.change]の時の動き
+    // MARK: imageViewのpanGestureのstatusが、[.change]の時の動き
     private func adjustPaddingChange(move: CGPoint) {
         let movingConstant = move.y / 30
-
-        if movingConstant <= 12 {
-            videoImageViewTrailingConstraint.constant = movingConstant
-            videoImageViewLeadingConstraint.constant = movingConstant
-            backViewTopConstraint.constant = movingConstant
-        }
         
+        if movingConstant <= 12 {
+            videoImageViewTrailingConstraint.constant = -movingConstant
+            videoImageViewLeadingConstraint.constant = movingConstant
+            
+            backViewTrailingConstraint.constant = -movingConstant
+        }
     }
     
     private func adjustHeightChange(move: CGPoint) {
-        //280（最大値）- 70(最小値) = 210
-        let parentViewHeight = self.view.frame.height
+        let parantViewHeight = self.view.frame.height
         let heightRatio = 210 / videoImageMaxY
         let moveHeight = move.y * heightRatio
         
@@ -129,7 +130,7 @@ class VideoViewController: UIViewController {
         videoImageViewHeightConstraint.constant = 280 - moveHeight
         describeViewTopConstraint.constant = move.y * 0.8
         
-        let bottomMoveY = parentViewHeight - videoImageMaxY
+        let bottomMoveY = parantViewHeight - videoImageMaxY
         let bottomMoveRatio = bottomMoveY / videoImageMaxY
         let bottomMoveConstant = move.y * bottomMoveRatio
         backViewBottomConstraint.constant = bottomMoveConstant
@@ -139,15 +140,14 @@ class VideoViewController: UIViewController {
         let alphaRatio = move.y / (self.view.frame.height / 2)
         describeView.alpha = 1 - alphaRatio
         baseBackGroundView.alpha = 1 - alphaRatio
-        
     }
     
     private func adjustWidthChange(move: CGPoint) {
         let originalWidth = self.view.frame.width
         let constant = originalWidth - move.y
         
-        if minimusImageViewTrailingConstant > constant {
-            videoImageViewTrailingConstraint.constant = minimusImageViewTrailingConstant
+        if minimumImageViewTrailingConstant > constant {
+            videoImageViewTrailingConstraint.constant = minimumImageViewTrailingConstant
             return
         }
         
@@ -156,70 +156,73 @@ class VideoViewController: UIViewController {
         }
     }
     
-    // MARK: imageViewのPanGestureのstatusが、[.ended]の時の動き
+    // MARK: imageViewのpanGestureのstatusが、[.ended]の時の動き
     private func imageViewEndedAnimation(move: CGPoint, imageView: UIImageView) {
         if move.y < self.view.frame.height / 3 {
-            
             UIView.animate(withDuration: 0.2, delay: 0, usingSpringWithDamping: 0.8, initialSpringVelocity: 0.8, options: [], animations: {
-                imageView.transform = .identity
-                self.backToIdentifyAllViews(imageView: imageView)
+                
+                self.backToIdentityAllViews(imageView: imageView)
             })
         } else {
-            UIView.animate(withDuration: 0.3, delay: 0, usingSpringWithDamping: 0.8, initialSpringVelocity: 0.8, options: []) {
+            UIView.animate(withDuration: 0.4, delay: 0, usingSpringWithDamping: 0.7, initialSpringVelocity: 0.7, options: []) {
+                
                 self.moveToBottom(imageView: imageView)
+                
             } completion: { _ in
                 
                 UIView.animate(withDuration: 0.2) {
+                    
                     self.videoImageView.isHidden = true
                     self.videoImageBackView.isHidden = true
                     
                     let image = self.videoImageView.image
                     let userInfo: [String: Any] = ["image": image, "videoImageMinY": self.videoImageView.frame.minY]
+                    
                     NotificationCenter.default.post(name: .init("thumbnailImage"), object: nil, userInfo: userInfo as [AnyHashable : Any])
+                    
                 } completion: { _ in
                     self.dismiss(animated: false, completion: nil)
-
                 }
             }
         }
     }
     
     private func moveToBottom(imageView: UIImageView) {
-        //imageViewの設定
+        // imageViewの設定
         imageView.transform = CGAffineTransform(translationX: 0, y: videoImageMaxY)
-        videoImageViewTrailingConstraint.constant = minimusImageViewTrailingConstant
+        videoImageViewTrailingConstraint.constant = minimumImageViewTrailingConstant
         videoImageViewHeightConstraint.constant = 70
         videoImageViewLeadingConstraint.constant = 12
         
         videoImageBackView.transform = CGAffineTransform(translationX: 0, y: videoImageMaxY)
-        backView.alpha = 0
         describeView.alpha = 0
+        backView.alpha = 0
+        baseBackGroundView.alpha = 0
+        
         self.view.layoutIfNeeded()
     }
     
-    private func backToIdentifyAllViews(imageView: UIImageView) {
-        //imageViewの設定
+    private func backToIdentityAllViews(imageView: UIImageView) {
+        // imageViewの設定
         imageView.transform = .identity
         videoImageBackView.transform = .identity
-        
         videoImageViewHeightConstraint.constant = 280
         videoImageViewLeadingConstraint.constant = 0
         videoImageViewTrailingConstraint.constant = 0
         
-        //backViewの設定
+        // backViewの設定
         backViewTrailingConstraint.constant = 0
-        backViewTopConstraint.constant = 0
         backViewBottomConstraint.constant = 0
-        backView.alpha = 0
+        backViewTopConstraint.constant = 0
+        backView.alpha = 1
         
-        //describeViewの設定
+        // describeViewの設定
         describeViewTopConstraint.constant = 0
         describeView.alpha = 1
-        backView.alpha = 0
+        
         baseBackGroundView.alpha = 1
         
         self.view.layoutIfNeeded()
-        
     }
     
 }
